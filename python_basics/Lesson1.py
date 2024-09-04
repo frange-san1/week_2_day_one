80bf2249be581cadec1ee35616651b74dc387efe
Rangel-Santos <frangel-san1@cps.k12.il.us>
2024-09-04 09:29:48 -0500

    first commit

---
+++ python_basics/Lesson1.py
@@ -0,0 +1,3 @@
+print("My name is Fabio")
+print("I'm in Computer Science")
+print("I like tacos")
---
+++ week2.py
@@ -0,0 +1,10 @@
+# ask someone what day it is 
+day = input("what day is it")
+print("Today is" + day)
+# ask them for their favorite holiday
+holiday = input("What is your favorite holiday")
+print("Your favorite holiday is" + holiday)
+#print out the day and their favroite holiday in
+#a sentence
+print("Today is",day, "and your favorite Holiday is" + holiday)
+
